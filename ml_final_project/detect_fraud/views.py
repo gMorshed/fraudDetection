27b39fe1f1f06abe23dff440a7b1bd424a1ce2da
Gazi Mahbub-Morshed <gmorshed@Gazis-MacBook-Pro.local>
2019-11-23 13:02:31 -0700

    styled form with error message for feature validation, file not csv, file too big

--- ml_final_project/detect_fraud/views.py
+++ ml_final_project/detect_fraud/views.py
@@ -3,45 +3,42 @@ import csv
 import io
 
 from django.contrib import messages
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 import pandas as pd
-
-from ml_final_project import settings
-from .models import *
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
 
 
 # Create your views here.
 # one parameter named request
 def profile_upload(request):
+    expected_features = ['step', 'type', 'amount', 'nameOrig', 'oldbalanceOrg', 'newbalanceOrig', 'nameDest',
+                         'oldbalanceDest',
+                         'newbalanceDest', 'isFraud', 'isFlaggedFraud']
     # declaring template
     template = "profile_upload.html"
-    data = Profile.objects.all()
-    # prompt is a context variable that can have different values      depending on their context
-    prompt = {
-        'order': 'Order of the CSV should be name, email, address,    phone, profile',
-        'profiles': data
-    }
     # GET request returns the value of the data with the specified key.
     if request.method == "GET":
-        return render(request, template, prompt)
-    csv_file = request.FILES.get(u'file')
+        return render(request, template)
+    csv_file = request.FILES.get(u'csv_file')
     # let's check if it is a csv file
     if not csv_file.name.endswith('.csv'):
-        messages.error(request, 'THIS IS NOT A CSV FILE')
-    data_set = csv_file.read().decode('UTF-8')
+        messages.error(request, 'File is not CSV type')
+        return render(request, template)
 
+    # if file is too large, return
+    if csv_file.multiple_chunks():
+        messages.error(request, "Uploaded file is too big (%.2f MB)." % (csv_file.size / (1000 * 1000),))
+        return render(request, template)
+
+    # now reading the data
+    data_set = csv_file.read().decode('UTF-8')
     features = []
     rows = []
     first_col = True
     for data in data_set.split('\n'):
         if first_col:
             for item in data.split(','):
-                features.append(item)
+                features.append(item.strip('\r'))
             first_col = False
         else:
             r = []
@@ -49,8 +46,16 @@ def profile_upload(request):
                 r.append(item)
             rows.append(r)
 
+    # feature validation
+    for feature in features:
+        if not (feature in expected_features):
+            print(feature in expected_features)
+            messages.error(request, 'File should not contain this column: %s' % feature)
+            messages.error(request, 'It should contain these columns: %s' % expected_features)
+            return render(request, template)
+
     df = pd.DataFrame(rows, columns=features, dtype=float)
     print(df.head())
 
     context = {}
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Dataframe looks good. Let's do machine learning.")
