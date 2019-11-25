63ed3fc100a603c3c0ac0ccb02bda2f14ea4175b
Garrett Stanford <gstanford@mymail.mines.edu>
2019-11-25 14:24:31 -0700

    Trained SOM on German dataset, achieving accuracy of only 69%, saved SOM model for synthetic dataset to synthetic_som.p, saved SOM model for German dataset to german_som.p

--- german_som.py
+++ german_som.py
@@ -11,12 +11,12 @@ import sklearn.metrics
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectKBest, mutual_info_regression, RFE
 
-# ******************** Train on synthetic dataset ******************************
-inputDataFrame = pd.read_csv('./data.csv')
+# *********************** Train on German dataset ******************************
+inputDataFrame = pd.read_csv('./german_data.csv')
 
-#split test train
-fraud_targets = pd.Series(inputDataFrame["isFraud"])
-inputDataFrame.drop(columns=["isFraud"], inplace=True)
+# Split test train
+fraud_targets = pd.Series(inputDataFrame["class"])
+inputDataFrame.drop(columns=["class"], inplace=True)
 fraud_features = pd.DataFrame(inputDataFrame)
 X_train, X_test, y_train, y_test = train_test_split(fraud_features, fraud_targets, test_size=0.2, random_state=0)
 
@@ -68,25 +68,5 @@ class_assignments = som.labels_map(X_train, y_train)
 print(sklearn.metrics.classification_report(y_test, classify(som, X_test, class_assignments)))
 
 # saving the som in the file som.p
-with open('synthetic_som.p', 'wb') as outfile:
+with open('german_som.p', 'wb') as outfile:
     pickle.dump(som, outfile)
-
-#feature selection
-# k=5
-# mi_transformer = SelectKBest(mutual_info_regression,k=k).fit(X_train, y_train)
-# mi_X_train,mi_X_test = mi_transformer.transform(X_train), mi_transformer.transform(X_test)
-#
-# for feature, importance in zip(fraud_features.columns, mi_transformer.scores_):
-#     print(f"The MI score for {feature} is {importance}")
-
-# train_data = np.column_stack((X_train, y_train))
-#
-# # Train SOM (Self-organizing-maps)
-# sm = sp.SOMFactory().build(train_data, normalization="var", initialization="random")
-# sm.train(n_job=1, verbose=False, train_rough_len=2, train_finetune_len=5)
-#
-# predicted = sm.predict(X_test, k=5, wt="distance")
-# print(predicted)
-#
-# print(f"The accuracy score is {sklearn.metrics.accuracy_score(y_test, predicted)}")
-# print(f"The f1 score is {sklearn.metrics.f1_score(y_test, predicted)}")
