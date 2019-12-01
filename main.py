4bfdf6012502fb5479745becf41fd567ac4bb93b
GAZI MAHBUB-MORSHED <gmdmahbubmorshed@mymail.mines.edu>
2019-12-01 00:13:16 -0700

    SVM finalized

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+testTran = pd.read_csv('transection_to_test_w_ground_truth.csv')
+testTran.head(30)
+testTran.drop(columns=['isFraud'],inplace=True)
 import numpy as np
 import io
 from sklearn.model_selection import train_test_split
@@ -8,9 +11,13 @@ from sklearn.svm import SVC # SVM classifier model
 from sklearn.linear_model import LinearRegression, LassoCV
 from sklearn.decomposition import PCA
 from joblib import dump,load
+from sklearn.metrics import confusion_matrix
+from sklearn import preprocessing
 
 
 
+number_of_fraud_trans = 500
+number_of_non_fraud_trans = 500
 inputDataFrame = pd.read_csv('../data.csv')
 inputDataFrame.dropna(inplace=True)
 assert inputDataFrame.isnull().sum().sum() == 0
@@ -21,20 +28,20 @@ def f(name):
     return 1
   else:
     return NaN
-
-
+  
+  
 inputDataFrame["isMerchantOrig"] = inputDataFrame["nameOrig"].apply(func=f)
 inputDataFrame["isMerchantDest"] = inputDataFrame["nameDest"].apply(func=f)
 inputDataFrame.drop(columns=["nameOrig","nameDest"], inplace=True)
 new_inputDataFrame = pd.get_dummies(data=inputDataFrame["type"])
 for item in new_inputDataFrame:
-  inputDataFrame.insert(2, item,new_inputDataFrame[item])
+  inputDataFrame.insert(2, item,new_inputDataFrame[item]) 
 inputDataFrame.drop(columns=["type","isFlaggedFraud"], inplace=True)
-
 #samplying the non fradulent with all fradulent data
 all_fraud = inputDataFrame.query('isFraud==1')
+all_fraud= all_fraud.sample(n=number_of_fraud_trans, random_state=1,replace=True)
 not_fraud = inputDataFrame.query('isFraud==0')
-not_fraud= not_fraud.sample(n=4000, random_state=1)
+not_fraud= not_fraud.sample(n=number_of_non_fraud_trans, random_state=1,replace=True)
 frames = [all_fraud,not_fraud]
 inputDataFrame = pd.concat(frames)
 
@@ -44,7 +51,8 @@ inputDataFrame.drop(columns=["isFraud"], inplace=True)
 fraud_features = pd.DataFrame(inputDataFrame)
 X_train, X_test, y_train, y_test = train_test_split(fraud_features, fraud_targets, test_size=0.2, random_state=0)
 
-
+X_train = preprocessing.scale(X_train)
+X_test = preprocessing.scale(X_test)
 
 
 #feature selection
@@ -52,6 +60,7 @@ k=5
 mi_transformer = SelectKBest(mutual_info_regression,k=k).fit(X_train, y_train)
 mi_X_train,mi_X_test = mi_transformer.transform(X_train), mi_transformer.transform(X_test)
 
+testTran_selected_feature = testTran[testTran.columns[mi_transformer.get_support()]]
 #code to make the test transection for the API
 # import sys
 # dataset = pd.DataFrame({'step': mi_X_test[:, 0], 'amount': mi_X_test[:, 1], 'oldbalanceOrg': mi_X_test[:, 2], 'newbalanceOrig': mi_X_test[:, 3], 'isMerchantDest': mi_X_test[:, 4]})
@@ -89,14 +98,20 @@ plt.show()
 #SVM model
 hyperparams = {
     "C": [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 1e2, 1e3, 1e4],
-    "random_state": [0]
+    "random_state": [0],
+    'kernel':['linear','rbf','poly']
 }
 svc = SVC(gamma='auto')
 clf = GridSearchCV(svc, hyperparams, scoring='accuracy',cv=5)
 clf.fit(mi_X_train, y_train)
-# testScore = clf.score(mi_X_test, y_test)
-# print("SVM + Feature Selection Accuracy: ", testScore)
-print(clf.predict(mi_X_test[0:11]))
+testScore = clf.score(mi_X_test, y_test)
+print("SVM + Feature Selection Accuracy: ", testScore)
+prediction_list = clf.predict(mi_X_test)
+tn, fp, fn, tp = confusion_matrix(y_test,prediction_list).ravel()
+print("tn, fp, fn, tp", tn, fp, fn, tp)
+print("best params",clf.best_params_)
+print(clf.predict(testTran_selected_feature.to_numpy()))
+dump(clf, 'svm_selected_feature_with_kernal.joblib')
 
 # optional = False
 # if(optional):
@@ -115,12 +130,16 @@ print(clf.predict(mi_X_test[0:11]))
 # print("SVM + Feature Extraction Accuracy: ", testScore2)
 
 
-# svc3 = SVC(gamma='auto')
-# clf3 = GridSearchCV(svc3, hyperparams, scoring='accuracy', cv=5)
-# clf3.fit(X_train, y_train)
-# testScore3 = clf3.score(X_test, y_test)
-# print("SVM with no Feature Selection/Extraction Accuracy: ", testScore3)
-
+svc3 = SVC(gamma='auto')
+clf3 = GridSearchCV(svc3, hyperparams, scoring='accuracy', cv=5)
+clf3.fit(X_train, y_train)
+testScore3 = clf3.score(X_test, y_test)
+print("SVM with no Feature Selection/Extraction Accuracy: ", testScore3)
+prediction_list = clf3.predict(X_test)
+tn, fp, fn, tp = confusion_matrix(y_test,prediction_list).ravel()
+print("tn, fp, fn, tp", tn, fp, fn, tp)
+print("best params",clf3.best_params_)
+print(clf3.predict(testTran.to_numpy()))
 
 #dumping into pickel
-dump(clf, 'svm_feature_selection_with_kernal.joblib')
+dump(clf3, 'svm_all_feature_with_kernal.joblib')
