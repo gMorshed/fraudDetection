223df14d106b0254e10851befef7f25cdd10f319
Gazi Mahbub-Morshed <gmorshed@Gazis-MacBook-Pro.local>
2019-11-30 17:26:22 -0700

    website mostly finalize, just need the models

--- ml_final_project/detect_fraud/views.py
+++ ml_final_project/detect_fraud/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 import pandas as pd
 from joblib import load
+from sklearn.metrics import classification_report, confusion_matrix
 
 
 def classify_fradulent(predict):
@@ -15,24 +16,39 @@ def classify_fradulent(predict):
     return "Non Fraudulent Transaction"
 
 
-def run_fraud_predict(dataframe):
-    clf = load('../random_forest_w_all_feature.joblib')
+def run_fraud_predict(dataframe, model):
+    clf = None
+    if model == "random":
+        clf = load('../random_forest_w_all_feature.joblib')
+    elif model == "svm":
+        clf = load('../svm_feature_selection_with_kernal.joblib')
+    elif model == "som":
+        clf = load('../random_forest_w_all_feature.joblib')
+    else:
+        clf = load('../random_forest_w_all_feature.joblib')
     dataframe.rename(columns={"isFraud": "isFraud_ground_truth"}, inplace=True)
     holding_ground_truth = dataframe['isFraud_ground_truth']
     dataframe.drop(columns=["isFraud_ground_truth"], inplace=True)
     prediction_list = clf.predict(dataframe.to_numpy())
     dataframe = dataframe.join(holding_ground_truth)
-    dataframe.insert(6, "fraud_prediction", prediction_list, True)
+    dataframe.insert(14, "fraud_prediction", prediction_list, True)
+    tn, fp, fn, tp = confusion_matrix(dataframe['isFraud_ground_truth'].to_numpy(), prediction_list).ravel()
+    confusion_matrix_simplify = ["True Negative: " + str(tn), "False Positive: " + str(fp),
+                                 "False Negative: " + str(fn), "True Positive: " + str(tp)]
+
+    report = classification_report(dataframe['isFraud_ground_truth'].to_numpy(), prediction_list,
+                                   target_names=['Non-fraud', 'Fraud'],output_dict=True)
     dataframe["fraud_prediction"] = dataframe["fraud_prediction"].apply(func=classify_fradulent)
     dataframe["isFraud_ground_truth"] = dataframe["isFraud_ground_truth"].apply(func=classify_fradulent)
-    return dataframe
+    return dataframe, confusion_matrix_simplify, report
 
 
 # Create your views here.
 # one parameter named request
 def profile_upload(request):
     expected_features = ['step', 'TRANSFER', 'PAYMENT', 'DEBIT', 'CASH_OUT', 'CASH_IN', 'amount', 'oldbalanceOrg',
-                         'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest', 'isMerchantOrig', 'isMerchantDest','isFraud']
+                         'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest', 'isMerchantOrig', 'isMerchantDest',
+                         'isFraud']
     # declaring template
     template = "profile_upload.html"
     # GET request returns the value of the data with the specified key.
@@ -73,15 +89,22 @@ def profile_upload(request):
             messages.error(request, 'It should contain these columns: %s' % expected_features)
             return render(request, template)
 
+    model  = request.POST['model']
     df = pd.DataFrame(rows, columns=features, dtype=float)
     df = df.dropna()
     # render dataframe as html
-    result_df = run_fraud_predict(df)
-
+    result_df, confusion_matrix_simplify, report = run_fraud_predict(df, model)
     html = result_df.to_html()
-
     # write html to file
-    text_file = open("templates/index.html", "w")
+    text_file = open("templates/table.html", "w")
     text_file.write(html)
     text_file.close()
-    return render(request, 'index.html')
+
+    report_dataframe = pd.DataFrame(report).transpose()
+    html = report_dataframe.to_html()
+    # write html to file
+    text_file = open("templates/report.html", "w")
+    text_file.write(html)
+    text_file.close()
+
+    return render(request, 'result.html', {'matrix_result': confusion_matrix_simplify})
